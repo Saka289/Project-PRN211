e9985c832b92569c7170093cb8204d8b89989103
Saka289 <saka289@icloud.com>
2023-03-23 23:44:06 +0700

    send mail

---
+++ PRN211_Project/Services/SendMail/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace PRN211_Project.Services.SendMail
+{
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(string to, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("sakanatsuma289@gmail.com"));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("sakanatsuma289@gmail.com", "gjervasogcknxqlv");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
---
+++ PRN211_Project/Services/SendMail/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using PRN211_Project.Models;
+using MailKit.Net.Smtp;
+
+namespace PRN211_Project.Services.SendMail
+{
+    public interface IEmailService
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+}
