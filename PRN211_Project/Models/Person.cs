35e842924600a9e039886c8d5bf8e8f769cf45a9
Saka289 <saka289@icloud.com>
2023-03-25 02:07:53 +0700

    update

--- PRN211_Project/Models/Person.cs
+++ PRN211_Project/Models/Person.cs
@@ -11,22 +11,22 @@ namespace PRN211_Project.Models
             Rates = new HashSet<Rate>();
         }
 
-        public int PersonId { get; set; }
+        public int? PersonId { get; set; }
         [Required]
 
-        public string Fullname { get; set; }
+        public string? Fullname { get; set; }
         [Required]
 
-        public string Gender { get; set; }
+        public string? Gender { get; set; }
         [Required]
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
         [Required]
 
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public int? Type { get; set; }
         public bool? IsActive { get; set; }
 
-        public virtual ICollection<Rate> Rates { get; set; }
+        public virtual ICollection<Rate>? Rates { get; set; }
     }
 }
