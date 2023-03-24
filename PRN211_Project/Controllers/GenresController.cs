48f7716c17a6b05431cd7451dd60fbeab5992df6
Huyismeee <tranhanhs172@gmail.com>
2023-03-25 00:44:15 +0700

    Huy Nguyen

---
+++ PRN211_Project/Controllers/GenresController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN211_Project.Models;
+using System.Collections.Specialized;
+
+namespace PRN211_Project.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult GenreManagement()
+        {
+            using (var context = new CenimaDBContext())
+            {
+                List<Genre> listG = context.Genres.ToList();
+                ViewBag.listG = listG;
+                if (TempData["shortMessage"] != null)
+                {
+                    ViewBag.Message = TempData["shortMessage"].ToString();
+                }
+
+                return View(listG);
+            }
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            using (var context = new CenimaDBContext())
+            {
+                Genre genre = context.Genres.SingleOrDefault(c => c.GenreId == id);
+                return PartialView("editGenre", genre);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Genre genre)
+        {
+            using (var context = new CenimaDBContext())
+            {
+                context.Update(genre);
+                context.SaveChanges();
+                return RedirectToAction("GenreManagement");
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            using (var context = new CenimaDBContext())
+            {
+                Genre genre = context.Genres.SingleOrDefault(c => c.GenreId == id);
+                return PartialView("deleteGenre", genre);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Genre genre)
+        {
+            using (var context = new CenimaDBContext())
+            {
+                Movie movie = context.Movies.FirstOrDefault(c => c.GenreId == genre.GenreId);
+                if (movie == null)
+                {
+                    context.Remove(genre);
+                    context.SaveChanges();
+                    return RedirectToAction("GenreManagement");
+                }
+                else
+                {
+                    TempData["shortMessage"] = "This Genre is currently in use";
+                    return RedirectToAction("GenreManagement");
+                }
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            using (var context = new CenimaDBContext())
+            {
+                string desc = this.Request.Form["des"];
+                Genre genre = new Genre()
+                {
+                    Description= desc
+                };
+                context.Genres.Add(genre);
+                context.SaveChanges();
+                return RedirectToAction("GenreManagement");
+            }
+        }
+    }
+}
---
+++ PRN211_Project/Controllers/ProductDetail.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PRN211_Project.Models;
+using System;
+using System.Text.Json;
+
+namespace PRN211_Project.Controllers
+{
+    public class ProductDetail : Controller
+    {
+        public IActionResult movieDetail(int movieId)
+        {
+            using (var context = new CenimaDBContext())
+            {
+                var movie = context.Movies.Include(m => m.Genre).Include(m => m.Rates).SingleOrDefault(m => m.MovieId == movieId);
+                List<Rate> Rate = context.Rates.Include(m => m.Person).Where(m => m.MovieId == movieId).ToList();
+                ViewBag.Movie = movie;
+                if (movie.Rates.Count > 0)
+                {
+                    ViewBag.NumericRate = movie.Rates.Average(r => r.NumericRating);
+                }
+                else
+                {
+                    ViewBag.NumericRate = "Not yet rated";
+                }
+
+                ViewBag.Rate = Rate;
+            }
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult comment(Rate rate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("movieDetail", new { movieId = rate.MovieId });
+            }
+            using (var context = new CenimaDBContext())
+            {
+                if (HttpContext.Session.GetString("account") != null)
+                {
+                    var user = (Person)JsonSerializer.Deserialize<Person>(HttpContext.Session.GetString("account"));
+                    var checkRate = context.Rates.FirstOrDefault(r => r.MovieId == rate.MovieId && r.PersonId == user.PersonId);
+                    if (checkRate != null)
+                    {
+                        checkRate.MovieId = rate.MovieId;
+                        checkRate.PersonId = user.PersonId;
+                        checkRate.NumericRating = rate.NumericRating;
+                        checkRate.Comment = rate.Comment;
+                        checkRate.Time = DateTime.Now;
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        rate.Time = DateTime.Now;
+                        rate.PersonId = user.PersonId;
+                        context.Rates.Add(rate);
+                        context.SaveChanges();
+                    }
+                }
+                return RedirectToAction("movieDetail", new { movieId = rate.MovieId, rated = rate });
+            }
+
+        }
+
+    }
+}
